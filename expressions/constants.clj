3edeee4be1f7a0963ef8ee6670d4973a0cb67efb
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-19 21:42:59 -0500

    Switched completely to new structure

---
+++ expressions/constants.clj
@@ -0,0 +1,26 @@
+(ns expressions.constants 
+  (:require
+   [flower.reflect :as reflect]))
+
+(ns expressions.constants)
+
+(def site-title "Michael McCord for Congress")
+(def district "GA 1st District")
+(def tagline "America First, for Real People")
+(def mission-statement "Washington is out of touch with rural Georgia, and Michael is running to fix it.")
+(def global-desc "Washington is out of touch with rural Georgia.")
+(def site-author "Philocalyst")
+(def lang "en")
+
+(def phone "(912) 483-8565")
+(def email "Michael@McCordforGA.com")
+(def address-line1 "PO Box 228")
+(def address-line2 "2126 East Victory Dr.")
+(def city-state-zip "Savannah GA 31404")
+
+(def donate-url "https://secure.actblue.com/donate/donatetomccordforcongress")
+
+(def nav-items
+  [{:href "#meet-michael" :text "Meet Michael"}
+   {:href "#platform" :text "Platform"}
+   {:href "#volunteer" :text "Volunteer"}])
